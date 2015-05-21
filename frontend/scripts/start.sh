9c96928bef91685c42814f372d9dc56e8c69fa6d
Scott Scoble <sscoble@codesmith.ws>
2015-05-20 20:07:29 -0700

    update

--- frontend/scripts/start.sh
+++ frontend/scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --net host --restart on-failure:3 -v /opt/todomvc:/var/www/html nginx
+docker run -d --net host --restart on-failure:3 -v /opt/todomvc:/var/www/html -p 80:80 nginx
